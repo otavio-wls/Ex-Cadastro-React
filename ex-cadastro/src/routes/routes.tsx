b5c6b870fd7f5fd622d5d3170e12d89ddcb9371e
otavio-wls <64418161+otavio-wls@users.noreply.github.com>
2020-08-21 14:46:09 -0300

    Feito uma verificação e validação que se o usuario não tiver um token ele é redirecionado para a tela de Login novamente

--- ex-cadastro/src/routes/routes.tsx
+++ ex-cadastro/src/routes/routes.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Login from '../pages/Login';
+import isLogin   from '../Component/Auth/auth';
 import Clients from '../pages/Client';
 import Cadastro from '../pages/Cadastro';
 import NotFound from '../pages/NotFound/index';
 import {history} from '../history';
+
+const PrivateRoute =({component: Component, ...rest}) => (
+  <Route
+    {...rest}
+    render={props => 
+      isLogin() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: '/', state: { from: props.location }}} />
+      )
+    } />
+);
 
 
 export default function Routes() {
@@ -13,7 +26,7 @@ export default function Routes() {
     <BrowserRouter history={history}>
       <Switch>
         <Route exact path='/' component={Login}/>
-        <Route exact path='/clients' component={Clients} /> 
+        <PrivateRoute exact path='/clients' component={Clients} /> 
         <Route exact path='/cadastro' component={Cadastro} />
         <Route component={NotFound} />
       </Switch>
