7ddc5133c65095c9d22f9ea74582010958ac54d4
otavio-wls <64418161+otavio-wls@users.noreply.github.com>
2020-08-21 09:55:23 -0300

    Alterado o código do rodapé para usar component do material UI

--- ex-cadastro/src/Component/Footer/index.tsx
+++ ex-cadastro/src/Component/Footer/index.tsx
@@ -1,20 +1,87 @@
-import './style.css';
 import React from 'react';
-import { FaYoutube, FaInstagram } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { CssBaseline, Container, Typography, makeStyles } from '@material-ui/core';
+// import { Facebook, Instagram, Twitter } from '@material-ui/icons';
 
-export default function Footer() {
-  return(
-    <footer className='footer'>
-      <div className='footer__social'>
-        <p>
-          <FaYoutube size="40" />
-        </p>
-        <p>
-          <FaInstagram size="40" />
-        </p>
-      </div>
-      <p className='footer__copyright'>Meu Copyright</p>
-    </footer>
-
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" style={{textAlign: 'center', color:'#ffffff'}}>
+      {'Copyright © '}
+      <Link style={{textDecoration: 'none'}} color="inherit" to="https://material-ui.com/">
+        Photografy Premium
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexGrow: 1,
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',    
+    backgroundColor: '#00008B',     
+  },  
+  // redesSociais:{
+  //   display: 'flex',
+  //   listStyle: 'none',
+  //   textDecoration: 'none',
+  //   color: '#ffffff',
+  //   marginLeft: '50%',
+  //   marginRight: '50%',
+  //   alignItems: 'center',
+  // }
+}));
+
+export default function Footer() {
+  const classes = useStyles();
+  return(
+    <div className={classes.root}>
+    <CssBaseline />
+    <footer className={classes.footer}>
+      <Container maxWidth="lg">
+        <Typography variant="body1" style={{textAlign: 'center', color:'#ffffff'}}>Photografy Premium</Typography>
+        <Copyright />
+        {/* <div className={classes.redesSociais}>
+          <a href='https://pt-br.facebook.com/'><p><Facebook/></p></a>
+          <a href='http://instagram.com/'><p><Instagram/></p></a>
+          <a href='https://twitter.com/login?lang=pt'><p><Twitter/></p></a>
+        </div> */}
+      </Container>
+    </footer>    
+  </div>
+  );
+}
+
+
+
+// import './style.css';
+// import React from 'react';
+// import { FaYoutube, FaInstagram } from 'react-icons/fa';
+
+// export default function Footer() {
+//   return(
+//     <footer className='footer'>
+//       <div className='footer__social'>
+//         <p>
+//           <FaYoutube size="40" />
+//         </p>
+//         <p>
+//           <FaInstagram size="40" />
+//         </p>
+//       </div>
+//       <p className='footer__copyright'>Meu Copyright</p>
+//     </footer>
+
+//   );
+// }
