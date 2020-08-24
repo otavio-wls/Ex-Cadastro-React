391b4eb017e1aa6f42cae574f41180870b301063
otavio-wls <64418161+otavio-wls@users.noreply.github.com>
2020-08-24 16:23:08 -0300

    Modificado a tela de cadastro, retirado header e footer e criado um card na lateral. Cores usadas também modificadas

--- ex-cadastro/src/pages/Cadastro/index.tsx
+++ ex-cadastro/src/pages/Cadastro/index.tsx
@@ -1,39 +1,53 @@
+import './style.css';
 import React, { useState, Fragment } from 'react';
 import api from '../../api/api';
 import { toast } from 'react-toastify';
-import { TextField, makeStyles, Grid, Typography, Button } from '@material-ui/core';
+import { TextField, makeStyles, Grid, Typography, Button, Card, CardContent } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
-      width: '85%',
-    },
+      width: '100%',
+    },   
+    '& html': {
+      background: '#000000'
+    }
+  },    
+  cardPrincipal:{
+    backgroundColor: '#1E90FF'
   },
   paper:{
-    width: '85%',
+    width: '75%',    
+    height: '15%',
     marginTop: '5px',    
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    marginLeft: '10%',    
     marginBottom: '10px',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start'
   },
   containerForm: {    
-    minWidth: '100vh',
-    flex: 1,
-    marginTop: 80,    
-    marginLeft: 5,
+    display: 'flex',
+    
+    marginRight: 'auto',
+    marginLeft: 'auto', 
+    maxWidth: '100%',        
+    marginTop: '5%',    
   },  
   buttons:{
     display: 'flex',        
   },
   submit:{
     margin: theme.spacing(3, 0, 2),
-    backgroundColor: '#00008B',
+    backgroundColor: '#1E90FF',
     color: '#fff',
   },  
+  description:{
+    width: '30%',
+    marginTop: '5.8%'
+  }
 }));
 
 export default function ValidationTextFields() {
@@ -41,8 +55,7 @@ export default function ValidationTextFields() {
 
   const [email, setEmail] = useState('');
   const [password, setPassWord] = useState('');
-  const [name, setName] = useState('');
-  const [address, setAddress] = useState('');
+  const [name, setName] = useState('');  
   const [contact, setContact] = useState('');
   const [contact2, setContact2] = useState(''); 
 
@@ -60,65 +73,69 @@ export default function ValidationTextFields() {
     }    
     setEmail('');
     setPassWord('');
-    setName('');
-    setAddress('');
+    setName('');    
     setContact('');
     setContact2('');
   } 
 
   return (
-    <Fragment>
-    
+    <Fragment>    
     <Grid container component='main' className={classes.containerForm}>     
-    <Grid item xs={10}  sm={ 'auto' }  md={ 'auto' }>
+    <Grid item xs={10}  sm={ 'auto' }  md={7} lg={7}>
       <div className={classes.paper}>
-        <Typography component='h1' variant='h4' style={{color: '#00008B'}}>Cadastro</Typography>
+        <Typography component='h1' variant='h4' style={{color: '#1E90FF'}}>Cadastro</Typography>
         <form>
           <TextField variant='outlined' margin='normal' required fullWidth id="nome" label="Nome"
             name="name"
+            color='primary'
             value={name}
-            onChange={e => setName(e.target.value)}
-            autoComplete="name"
+            onChange={e => setName(e.target.value)}            
             autoFocus/>
           <TextField variant='outlined' margin='normal' required fullWidth id="email" label="Email"
             name="email"
             value={email}
-            onChange={e => setEmail(e.target.value)}
-            autoComplete="email"
+            onChange={e => setEmail(e.target.value)}            
             autoFocus/>
           <TextField variant='outlined' margin='normal' type='password' required fullWidth id="senha" label="Senha"
             name="senha"
             value={password}
-            onChange={e => setPassWord(e.target.value)}
-            autoComplete="senha"
-            autoFocus/>
-          <TextField variant='outlined' margin='normal' required fullWidth id="endereco" label="Endereço"
-            name="endereco"
-            value={address}
-            onChange={e => setAddress(e.target.value)}
-            autoComplete="endereco"
-            autoFocus/>                
-          <TextField variant='outlined' margin='normal'  type='number' required  id="telefone" label="Telefone"
+            onChange={e => setPassWord(e.target.value)}            
+            autoFocus/>          
+          <TextField variant='outlined' margin='normal'  type='number' fullWidth required  id="telefone" label="Telefone"
             name="telefone"
             value={contact}
-            onChange={e => setContact(e.target.value)}
-            autoComplete="telefone"
+            onChange={e => setContact(e.target.value)}            
             style={{marginRight: '5px'}}
             autoFocus/>                
-          <TextField variant='outlined' margin='normal'  type='number' required   id="celular" label="Celular"
+          <TextField variant='outlined' margin='normal'  type='number' required fullWidth  id="celular" label="Celular"
             name="celular"
             value={contact2}
             onChange={e => setContact2(e.target.value)}
-            style={{marginRight: '5px'}}          
-            autoComplete= "celular"
+            style={{marginRight: '5px'}}                      
             autoFocus/>                
         </form>      
         <div className={classes.buttons}>
           <Button onClick={cadastrar} type='submit' fullWidth variant='contained'  className={classes.submit}>Cadastrar</Button>         
-        </div>        
-      </div>       
+        </div>                
+      </div>             
+      </Grid>        
+      <Grid item className={classes.description}>
+        <Card className={classes.cardPrincipal}>
+          <CardContent>        
+            <h1 style={{marginBottom: '25px', color: '#ffffff', fontWeight: 'bold'}}>Photografy Premium</h1>
+            <h3 style={{marginBottom: '15px', color: '#ffffff', fontWeight: 400}}>Cadastre-se já para poder aproveitar todos os benefícios da nossa plataforma</h3>
+            <p style={{marginBottom: '15px', color: '#ffffff'}}>Com seu cadastro você pode:</p>
+
+            <ul style={{marginBottom: '15px', marginLeft: '30px'}}>
+              <li style={{listStyleType: 'circle', color: '#ffffff'}}>Acesso a fotografias de diversas pessoas ao redor do mundo</li>
+              <li style={{listStyleType: 'circle', color: '#ffffff'}}>Encontrar amigos e conhecer novas pessoas no nosso chat</li>
+              <li style={{listStyleType: 'circle', color: '#ffffff'}}>Compartilhar suas fotos quantas vezes quiser </li>
+            </ul>
+          </CardContent>
+          <Link style={{textDecoration: 'none'}} to={'/'}><Button style={{marginLeft: '20px', color: '#fff'}}>Voltar para o login</Button></Link>
+        </Card>
       </Grid>
-    </Grid>    
+    </Grid>
   </Fragment>
   );
 }
