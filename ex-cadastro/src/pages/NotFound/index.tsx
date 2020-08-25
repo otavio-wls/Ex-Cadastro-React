defbb08180f0ea18c9d2aa074a378526befbddd2
otavio-wls <64418161+otavio-wls@users.noreply.github.com>
2020-08-25 10:46:33 -0300

    Modificada toda a página notFound, desde as imagens até a cor do body

--- ex-cadastro/src/pages/NotFound/index.tsx
+++ ex-cadastro/src/pages/NotFound/index.tsx
@@ -1,56 +1,30 @@
 import './style.css';
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
-import { Link } from 'react-router-dom';
-import { Button } from '@material-ui/core';
+import { Container, makeStyles, Button, Box } from '@material-ui/core';
 import lupa from '../../assets/lupa.png';
-import wifi from '../../assets/wifi.png';
 
-const useStyles = makeStyles((theme) =>({  
-  divContainer:{
-    width: '95%',    
-    fontFamily: 'ultra, serif',
-    justifyContent: 'space-between',
-    marginTop: '85px',    
-    display: 'flex',
-  },
-  text:{
+const useStyles = makeStyles((theme) => ({
+  button:{
+    marginLeft: '25%',
+    marginRight: '25%',
     textAlign: 'center',
-    fontSize: '45px',
-    color: '#ff0000',
-  },
-  link:{
-    width: '100%',    
-    marginRight: '50%'
-  },
-  lupa:{
-    width: '350px',
-    height: '350px',
-    marginTop: '10px',    
-  },
-  wifi:{
-    width: '175px',
-    height: "156px",
-    marginTop: '139px',    
   }
 }));
 
 export default function NotFound(){
   const classes = useStyles();
   return(
-    <div style={{background: '#000', height: '100vh', width: '100%'}}>    
-    <div style={{width: '100%', alignItems: 'center'}}>
-      <div>
-        <h1 className={classes.text}>Erro 404: Página não encontrada</h1>                
-        <Link to='/'><Button className={classes.link} >
-          Voltar para o inicio
-        </Button></Link>
-      </div>        
-      <div className={classes.divContainer}>     
-        <img src={lupa} alt='lupa' className={classes.lupa} />
-        <img src={wifi} alt='wifi' className={classes.wifi} />
-      </div>      
-    </div>
+    <div style={{background: '#191970', width: '100%', height: '100vh'}}>    
+    <Container maxWidth='md' style={{background: '#191970', width: '100%', height: '100%'}}>     
+      <Box>
+        <h2 style={{fontSize: '150px', textAlign: 'center', color: '#FF8C00'}}>4 0 4</h2>
+        <h1 style={{textAlign: 'center', color: '#ffffff', marginBottom: '2%'}}>Página não encontrada</h1>
+      </Box>      
+      <Box className={classes.button}>
+        <Button style={{color: 'white'}}>Voltar ao login</Button>
+      </Box>      
+      <img src={lupa} alt='lupa' style={{width: '280px', height: '300px'}} />
+    </Container>
     </div>
   );
 }
