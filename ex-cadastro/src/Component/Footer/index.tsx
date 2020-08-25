50dd97f649cb1c659e1a01ec4fae558906fc1989
otavio-wls <64418161+otavio-wls@users.noreply.github.com>
2020-08-25 15:27:08 -0300

    Refeito o footer do zero

--- ex-cadastro/src/Component/Footer/index.tsx
+++ ex-cadastro/src/Component/Footer/index.tsx
@@ -1,55 +1,72 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { CssBaseline, Container, Typography, makeStyles } from '@material-ui/core';
-// import { Facebook, Instagram, Twitter } from '@material-ui/icons';
+import { Grid, makeStyles } from '@material-ui/core';
+import { Facebook, Instagram, Twitter, LocationOn,Phone, Email, Copyright } from '@material-ui/icons';
+import './style.css';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" style={{textAlign: 'center', color:'#ffffff'}}>
-      {'Copyright © '}
-      <Link style={{textDecoration: 'none'}} color="inherit" to="/">
-        Photografy Premium
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
+const useStyles = makeStyles((theme) =>({
+  divFooter:{
+    display: 'flex',    
+    backgroundColor: '#1E90FF',
+    textAlign: 'center',
+    height: 'auto',
+    color: '#fff',    
+  },
+  location:{
     display: 'flex',
-    flexGrow: 1,
     flexDirection: 'column',
-    minHeight: '100vh',
+    marginLeft: '20%',
+    marginTop: '10%',
+    justifyContent: 'space-evenly'
   },
-  main: {
-    marginTop: theme.spacing(8),
-    marginBottom: theme.spacing(2),
-  },
-  footer: {
-    padding: theme.spacing(3, 2),
-    marginTop: 'auto',    
-    backgroundColor: '#1E90FF',     
-  },    
+  redesSociais:{
+    display: 'flex',
+    marginTop: '25px',
+    width: '50%',
+    justifyContent: 'space-evenly'
+  }
 }));
 
 export default function Footer() {
   const classes = useStyles();
   return(
-    <div className={classes.root}>
-    <CssBaseline />
-    <footer className={classes.footer}>
-      <Container maxWidth="lg">
-        <Typography variant="body1" style={{textAlign: 'center', color:'#ffffff'}}>Photografy Premium</Typography>
-        <Copyright />
-        {/* <div className={classes.redesSociais}>
-          <a href='https://pt-br.facebook.com/'><p><Facebook/></p></a>
-          <a href='http://instagram.com/'><p><Instagram/></p></a>
-          <a href='https://twitter.com/login?lang=pt'><p><Twitter/></p></a>
-        </div> */}
-      </Container>
-    </footer>    
-  </div>
+    <div>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <div className={classes.divFooter}>            
+            <div style={{width: '40%'}}>              
+              <div className={classes.location}>
+                <div style={{display: 'flex', alignItems: 'flexStart'}}>
+                  <LocationOn style={{fontSize: 35, marginBottom: '12px'}}/> 
+                  <p style={{textAlign: 'left', marginLeft: '5px'}}>Rua São Sebastião, 654 - Centro - Sorocaba,<br></br> São Paulo - Brasil</p>
+                </div>
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                  <Phone style={{fontSize: 35, marginBottom: '12px'}}/>
+                  <p style={{textAlign: 'left', fontSize: '18px', marginLeft: '5px'}}>+55 15 95148-5252 </p>
+                </div>
+                <div style={{display: 'flex', alignItems: 'flex-start'}}>
+                  <Email style={{fontSize: 35, marginBottom: '12px'}}/>
+                  <p style={{textAlign: 'left', fontSize: '15px', marginTop: '6px', marginLeft: '5px'}}>photografypremium@oficial.com</p>
+                </div>                
+              </div>              
+            </div>  
+            <div style={{width: '40%', textAlign: 'justify', marginLeft: 'auto'}}>
+              <div style={{width: '100%'}}>
+                <h2 style={{paddingTop: '7%', marginBottom: '10px'}}>Sobre nossa equipe</h2>
+                <p style={{width: '50%'}}>Empresa de fotografias profissionais!<br></br>Desde ensaio até criações de artes para seu evento!</p>
+              </div>
+              <div className={classes.redesSociais}>
+                <Facebook />
+                <Instagram />
+                <Twitter />
+              </div>
+            </div>
+          </div>          
+          <div style={{textAlign: 'center', marginTop: '5px', display: 'flex'}}>
+            <Copyright />
+            <p>Photografy Premium 2020</p>
+          </div>
+        </Grid>
+      </Grid>      
+    </div>
   );
 }
